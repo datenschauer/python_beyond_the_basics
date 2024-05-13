b8bda5ea9687290007d726b391736ce60f68212b
Stefan Böhringer <stefan@datenschauer.de>
2024-05-13 22:08:59 +0200

    update

--- pygame_test/settings.py
+++ pygame_test/settings.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 from pathlib import Path
 
 BASE_PATH = Path(__file__).resolve().parent
+IMAGES = BASE_PATH / 'images'
+SOUNDS = BASE_PATH / 'sounds'
 
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 800
